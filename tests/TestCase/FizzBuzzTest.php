8ea170d0b053c5a571db403e4bea83d60e13588f
Koji Tanaka <tenkoma@gmail.com>
2017-06-21 23:20:33 +0900

    implement when 2

--- tests/TestCase/FizzBuzzTest.php
+++ tests/TestCase/FizzBuzzTest.php
@@ -8,5 +8,6 @@ class FizzBuzzTest extends TestCase
     public function testMakeFizzBuzz()
     {
         $this->assertSame('1', FizzBuzz::makeFizzBuzz(1));
+        $this->assertSame('2', FizzBuzz::makeFizzBuzz(2));
     }
 }
